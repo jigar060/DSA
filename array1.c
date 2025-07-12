d5fa54cdf457e9cb4eea14e7e674410eeb98925c
jigar060 <jigarb453@gmail.com>
2025-07-12 12:08:23 +0530

    Create array1.c

---
+++ array1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+  int marks[10];
+  
+  for(int i=0;i<=9;i++)
+    {
+     scanf("%d",&marks[i]);
+    }
+   int countPass=0;
+   int countFail=0; 
+   
+   for(int i=0; i<10; i++)
+   {
+       if(marks[i]>=35)
+       {
+           countPass++;
+       }
+       else{
+             countFail++;
+           }
+       
+   }
+ printf("Total pass %d\n",countPass);
+ printf("Total fail %d\n",countFail);
+}
